1e45ad92293ec3d859fdeb5e621d22a057df9997
Lockley139 <98176888+Lockley139@users.noreply.github.com>
2025-02-18 19:22:22 +0000

    add footsteps again

--- Control.cs
+++ Control.cs
@@ -3,28 +3,28 @@ using System;
 
 public partial class Control : Node
 {
-    [Export] private Label LabelFPS;
+	[Export] private Label LabelFPS;
 
-    public override void _Ready()
-    {
-        Input.MouseMode = Input.MouseModeEnum.Captured;
-    }
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        if (Input.IsActionJustPressed("escape"))
-        {
-            Input.MouseMode = Input.MouseModeEnum.Visible;
-        }
+	public override void _Input(InputEvent @event)
+	{
+		if (Input.IsActionJustPressed("escape"))
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
 
-        if (Input.IsActionJustPressed("select"))
-        {
-            Input.MouseMode = Input.MouseModeEnum.Captured;
-        }
-    }
+		if (Input.IsActionJustPressed("select"))
+		{
+			Input.MouseMode = Input.MouseModeEnum.Captured;
+		}
+	}
 
-    public override void _Process(double delta)
-    {
-        LabelFPS.Text = $"FPS: {Engine.GetFramesPerSecond()}";
-    }
+	public override void _Process(double delta)
+	{
+		LabelFPS.Text = $"FPS: {Engine.GetFramesPerSecond()}";
+	}
 }
