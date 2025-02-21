acc20062d9f734419efb6199a92e0d204b4520cb
ReasonMakes <vaysym@gmail.com>
2025-02-21 06:42:24 -0800

    Re-push: "Adjust Greybox, Add Panels to Level, Add Cockpit Ambience"
    Greybox: Add shortcut between reactor and cooler that can only be slid through
    
    Modelling: Add floor panels
    
    Modelling: Break panel rotation... Oops
    
    Audio: Add cockpit spatial ambience node

--- robot/Robot.cs
+++ robot/Robot.cs
@@ -1,6 +1,38 @@
 using Godot;
 using System;
 
-public partial class Robot : Node
+public partial class Robot : CharacterBody3D
 {
+    [Export] private NavigationAgent3D NavAgent;
+    private float Speed = 2000f;
+    private float StopDistance = 5f;
+
+    public override void _PhysicsProcess(double deltaDouble)
+    {
+        float delta = (float)deltaDouble;
+
+        //Navigation needs to use the 1st frame to sync, so we only begin on the 2nd frame
+        if (Engine.GetPhysicsFrames() >= 2)
+        {
+            Control control = GetNode<Control>(GetTree().Root.GetChild(0).GetPath());
+
+            Vector3 distanceDirect = (control.Player.GlobalPosition - GlobalPosition);
+
+            if (distanceDirect.Length() >= StopDistance)
+            {
+                NavAgent.TargetPosition = control.Player.GlobalPosition;
+            }
+            else
+            {
+                NavAgent.TargetPosition = GlobalPosition;
+            }
+
+            Vector3 direction = (NavAgent.GetNextPathPosition() - GlobalPosition).Normalized();
+
+            float magnitude = Speed * delta;
+            Velocity = direction * magnitude;
+
+            MoveAndSlide();
+        }
+    }
 }
