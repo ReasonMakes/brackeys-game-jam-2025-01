c479cf2ae31265b7ee99a47753174e5dc25b1f11
ReasonMakes <vaysym@gmail.com>
2025-02-21 07:45:15 -0800

    Improve Music

--- music/Music.cs
+++ music/Music.cs
@@ -1,15 +1,38 @@
 using Godot;
 using System;
 
-public partial class Music : AudioStreamPlayer
+public partial class Music : Node3D
 {
 
 	[Export] private bool SettingMusicOn = true;
-	public override void _Process(double delta)
+
+	[Export] private AudioStreamPlayer StreamNonCombat;
+	[Export] private AudioStreamPlayer StreamCombat;
+
+    private AudioStreamPlayer ActiveStream;
+
+    [Export(PropertyHint.Range, "-200,0,")] private float VolumeAll = 0f;
+    [Export(PropertyHint.Range, "0,1,")] private float VolumeMultiplierNonCombat = 1f;
+    [Export(PropertyHint.Range, "0,1,")] private float VolumeMultiplierCombat = 1f;
+
+    public override void _Ready()
+    {
+        //Set the volume
+        StreamNonCombat.VolumeDb = VolumeAll * VolumeMultiplierCombat;
+        StreamCombat.VolumeDb = VolumeAll * VolumeMultiplierCombat;
+
+        //Start with non-combat music
+        ActiveStream = StreamCombat;
+
+        //Force first song to be element 0
+
+    }
+
+    public override void _Process(double delta)
 	{
-		if (!Playing && SettingMusicOn)
+		if (!ActiveStream.Playing && SettingMusicOn)
 		{
-			Play();
+            ActiveStream.Play();
 		}
 	}
 }
